b6ca9888f933ab325a0d673cdba1b61858580a8b
Robert Schuler <robes@users.noreply.github.com>
2015-07-24 20:05:59 -0700

    initial commit

---
+++ Makefile
@@ -0,0 +1,106 @@
+# A simple example of using Makefile in a JavaScript project
+
+# Phony targets
+.PHONY : all build test clean distclean deps help usage update
+
+# Disable built-in rules
+.SUFFIXES:
+
+# Subdir for node module dependencies
+MODULES=node_modules
+# Subdir for local node bin tools
+BIN=$(MODULES)/.bin
+# Subdir for bower front end component dependencies
+BOWER=bower_components
+
+# Project name
+NAME=ermrest
+# Distribution directory
+DIST=dist
+# Documentation directory
+DOC=doc
+# JavaScript source directory
+JS=js
+# Output package
+PKG=$(DIST)/$(NAME).js
+MIN=$(DIST)/$(NAME).min.js
+
+LINT=.make-lint
+
+# List of JS source files
+SOURCE=$(JS)/main.js
+
+# Dependencies
+DEPS=$(BOWER)/jquery/dist/jquery.js \
+	 $(BOWER)/jquery/dist/jquery.min.js \
+	 $(BOWER)/jquery/dist/jquery.min.map
+
+# "all" should build the package
+all: build docs
+
+# Makes the full source package
+$(PKG): $(SOURCE) $(LINT) $(DIST)
+	cat $(SOURCE) > $(PKG)
+
+# Makes the minified package (using 'closurecompiler')
+$(MIN): $(SOURCE) $(LINT) $(DIST) $(BIN)
+	$(BIN)/ccjs $(SOURCE) > $(MIN)
+
+# Lints the changed source (using 'jshint')
+$(LINT): $(SOURCE) $(BIN)
+	$(BIN)/jshint $?
+	@touch $(LINT)
+
+lint: $(LINT)
+
+build: $(PKG) $(MIN)
+
+docs: $(DOC)
+
+$(DOC): $(SOURCE) $(BIN)
+	$(BIN)/jsdoc --pedantic -d $(DOC) $(SOURCE)
+	@touch $(DOC)
+
+$(DIST):
+	mkdir -p $(DIST)
+
+# Installs Node modules locally
+$(BIN): $(MODULES)
+
+$(MODULES): package.json
+	npm install
+	@touch $(MODULES)
+
+# Installs Bower front end dependencies locally
+$(BOWER): $(BIN) bower.json
+	$(BIN)/bower install
+	@touch $(BOWER)
+
+deps: $(BIN) $(BOWER)
+
+update:
+	npm update
+	$(BIN)/bower update
+
+clean:
+	rm -rf $(DIST)
+	rm -rf $(DOC)
+	rm -f .make-*
+
+distclean: clean
+	rm -rf $(MODULES)
+	rm -rf $(BOWER)
+
+test:
+	@echo "Test invocation goes here"
+	@exit 1
+
+help: usage
+usage:
+	@echo "'make <target>' where <target> may be:"
+	@echo "    all       - an alias for build"
+	@echo "    build     - builds the package"
+	@echo "    test      - runs tests"
+	@echo "    deps      - local install of node and bower dependencies"
+	@echo "    clean     - cleans the dist dir"
+	@echo "    distclean - cleans the dist dir and the dependencies"
---
+++ js/main.js
@@ -0,0 +1,142 @@
+/**
+ * The root namespace for the ERMrest library.
+ * @namespace ermrest
+ */
+var ermrest = (function () {
+
+    /**
+     * ERMrest Service Provider. This is a stub. An example implementation
+     * of the ServiceProvider is the RestServiceProvider which implements the
+     * ERMrest "REST" style protocol.
+     *
+     * @constructor
+     * @this {ServiceProvider}
+     * @memberof ermrest
+     */
+    ServiceProvider = function() {};
+
+    /**
+     * An ERMrest service endpoint. This is a stub. An example implementation
+     * of the ServiceEndpoint is the RestServiceEndpoint which implements the
+     * state of a connection to an ERMrest service.
+     *
+     * @constructor
+     * @this {ServiceEndpoint}
+     * @memberof ermrest
+     */
+    ServiceEndpoint = function() {};
+
+    /**
+     * Returns a {Catalog} instance located at this {ServiceEndpoint}. This
+     * function returns immediately.
+     *
+     * @function
+     * @this {ServiceEndpoint}
+     * @param {String} name The name of the catalog.
+     * @param {function} statusfn The status callback function.
+     */
+    ServiceEndpoint.prototype.catalog = function (name) {};
+
+    /**
+     * Creates an instance of Catalog.
+     *
+     * @memberof ermrest
+     * @constructor
+     * @this {Catalog}
+     * @param {string} URL The URL of the catalog
+     */
+    Catalog = function(URL) {};
+
+    /**
+     * Creates the Catalog.
+     *
+     * @function
+     * @this {Catalog}
+     * @param {function} status A callback function
+     */
+    Catalog.prototype.create = function (status) {};
+    Catalog.prototype.get = function (status) {};
+    Catalog.prototype.destroy = function (status) {};
+
+    /**
+     * Returns the schemas interface for this Catalog.
+     *
+     * @this {Catalog}
+     * @return {Object} schema interface
+     */
+    Catalog.prototype.schemas = function() {};
+
+    /**
+     * Returns the entity interface for this Catalog.
+     *
+     * @this {Catalog}
+     * @return {Object} entity interface
+     */
+    Catalog.prototype.entity = function () {};
+
+    /**
+     * Creates an Entity instance.
+     *
+     * @memberof ermrest
+     * @constructor
+     * @this {Entity}
+     */
+    Entity = function () {};
+
+    /**
+     * Creates an Alias out of a Table for use within the context of an
+     * instance of the Entity interface.
+     *
+     * @function
+     * @this {Entity}
+     * @param {Table} table The table to alias
+     * @return {Alias} An alias based on the table
+     */
+    Entity.prototype.alias = function (table) {};
+
+    /**
+     * Extends the current ERM path by a Table or Alias. This method must 
+     * be called at least once before any operations may be performed.
+     *
+     * @function
+     * @this {Entity}
+     * @param {Table} table The table or table alias to extend the path
+     * @return {Entity} This entity instance.
+     */
+    Entity.prototype.extend = function (table) {};
+
+    /**
+     * Apply a filter to the current ERM path.
+     * @function
+     * @this {Entity}
+     */
+    Entity.prototype.filter = function () {};
+
+    /**
+     * Creates and returns a Condition object for use in filters.
+     * @function
+     * @this {Entity}
+     */
+    Entity.prototype.condition = function () {};
+
+    /**
+     * @constructor
+     * @this {Condition}
+     */
+    Entity.prototype.Condition = function () {};
+    Entity.prototype.Condition.prototype.predicate = function () {};
+    Entity.prototype.Condition.prototype.and = function () {};
+    Entity.prototype.Condition.prototype.or = function () {};
+
+    Entity.prototype.get = function () {};
+    Entity.prototype.update = function () {};
+    Entity.prototype.del = function () {};
+
+    /**
+     * Registered service providers.
+     * @var providers The dictionary of known service providers.
+     */
+    providers_ = { };
+
+    return { providers: providers_ };
+})();
