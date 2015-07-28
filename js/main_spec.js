aa7e151251ff5bfceb20629636e1a32e949b1bae
Robert Schuler <robes@users.noreply.github.com>
2015-07-27 17:27:27 -0700

    wiring in unit test framework

---
+++ js/main_spec.js
@@ -0,0 +1,10 @@
+function hello() { return 'hello world'; }
+
+describe('hello', function(){
+    it('should say hello', function(){
+        expect(hello()).to.be('hello world');
+    });
+    it('should say hello to person', function(){
+        expect(hello('Bob')).to.be('hello Bob');
+    });
+});
