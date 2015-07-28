e89f91c61d4e0cf5c921c0fba0df8c71da3986c3
Robert Schuler <robes@users.noreply.github.com>
2015-07-28 15:12:57 -0700

    more meaningful tests

--- js/main_spec.js
+++ js/main_spec.js
@@ -1,36 +1,38 @@
+"use strict";
+
 if (typeof chai == 'undefined')
+    // Works in 'mocha' for command line testing
     var expect = require('chai').expect;
 else
+    // Works in 'testem' for browser based testing
     var expect = chai.expect;
 
 describe('ERMrest', function(){
 
-    it('ERMrest should exist', function(){
+    it('Promise is available', function() {
+        expect(Promise).to.exist;
+    });
+
+    it('ERMrest namespace should exist', function(){
         expect(ERMrest).to.exist;
     });
 
-    it('ERMrest.rest should exist', function(){
+    it('ERMrest.rest provider should exist', function(){
         expect(ERMrest.rest).to.exist;
     });
 
-    it('ERMrest.rest(url) should exist', function(){
-        expect(ERMrest.rest('http://localhost/ermrest')).to.exist;
+    it('ERMrest.rest(url) should return an ERMrest.Catalog instance', function(){
+        expect(typeof ERMrest.rest('https://localhost/ermrest') == 'ERMrest.Catalog').to.exist;
     });
 
-    it('Catalog should exist', function(){
-        var service = ERMrest.rest('http://localhost/ermrest');
-        var catalog = service.catalog('fubar');
-        expect(catalog).to.exist;
-    });
-
-    it('Schema should exist', function(){
+    it('ERMrest.rest(url).catalog(name) should return an ERMrest.Catalog.Schema instance', function(){
         var service = ERMrest.rest('http://localhost/ermrest');
         var catalog = service.catalog('fubar');
         var schema  = catalog.schema('foo');
-        expect(schema).to.exist;
+        expect(typeof schema == 'ERMrest.Catalog.Schema').to.exist;
     });
 
-    it('Schema initialization work', function(){
+    it('Schema should be initialized', function(){
         var service = ERMrest.rest('http://localhost/ermrest');
         var catalog = service.catalog('fubar');
         var schema  = catalog.schema('foo');
