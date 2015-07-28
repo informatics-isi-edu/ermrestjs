154089a3fde8cdb577f7f85867322f4b8ad7e8f3
robes <robes@users.noreply.github.com>
2015-07-27 23:34:18 -0700

    a few working tests added

--- js/main_spec.js
+++ js/main_spec.js
@@ -1,10 +1,41 @@
-function hello() { return 'hello world'; }
+if (chai)
+    var expect = chai.expect;
+else
+    var expect = require('chai').expect;
 
-describe('hello', function(){
-    it('should say hello', function(){
-        expect(hello()).to.be('hello world');
+describe('ERMrest', function(){
+
+    it('ERMrest should exist', function(){
+        expect(ERMrest).to.exist;
     });
-    it('should say hello to person', function(){
-        expect(hello('Bob')).to.be('hello Bob');
+
+    it('ERMrest.rest should exist', function(){
+        expect(ERMrest.rest).to.exist;
+    });
+
+    it('ERMrest.rest(url) should exist', function(){
+        expect(ERMrest.rest('http://localhost/ermrest')).to.exist;
+    });
+
+    it('Catalog should exist', function(){
+        var service = ERMrest.rest('http://localhost/ermrest');
+        var catalog = service.catalog('fubar');
+        expect(catalog).to.exist;
+    });
+
+    it('Schema should exist', function(){
+        var service = ERMrest.rest('http://localhost/ermrest');
+        var catalog = service.catalog('fubar');
+        var schema  = catalog.schema('foo');
+        expect(schema).to.exist;
+    });
+
+    it('Schema initialization work', function(){
+        var service = ERMrest.rest('http://localhost/ermrest');
+        var catalog = service.catalog('fubar');
+        var schema  = catalog.schema('foo');
+        expect(schema.name).to.exist;
+        expect(schema.catalog).to.not.exist;
     });
 });
+
--- js/test.js
+++
@@ -1,8 +0,0 @@
-console.log(ERMrest);
-console.log(ERMrest.rest('http://localhost'));
-var svc = ERMrest.rest('http://localhost');
-var cat = svc.catalog('fubar');
-console.log(cat);
-console.log(cat.name);
-var sch = cat.schema('foo');
-console.log(sch);
