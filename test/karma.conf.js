0513371266a6de0f974f5447d94c0061e4d41dbf
Shuai Ma <mashuaihh@hotmail.com>
2016-03-01 16:17:14 -0800

    Added tweaks in .travis and karma.conf.js to fix issues in Travis CI running Chrome

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,79 +1,89 @@
 // Karma configuration
 // Generated on Fri Feb 19 2016 17:55:43 GMT-0800 (PST)
 
-module.exports = function(config) {
-  config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '../',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+module.exports = function (config) {
+    var configuration = {
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '../',
 
 
-    // list of files / patterns to load in the browser
-    files: [
-        'bower_components/angular/angular.js',
-        'bower_components/angular-mocks/angular-mocks.js',
-        //jquery, jasmine-jquery are here only for purpose of including mock json data
-        //now it's using approach suggested here:
-        // http://stackoverflow.com/questions/17370427/loading-a-mock-json-file-within-karmaangularjs-test
-        'bower_components/jquery/dist/jquery.min.js',
-        'node_modules/jasmine-jquery/lib/jasmine-jquery.js',
-        'js/*.js',
-        'test/*.js',
-        // fixtures to include mock data
-        {pattern: 'test/mock_data/*.json', watched: true, served: true, included: false}
-    ],
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine'],
 
 
-    // list of files to exclude
-    exclude: [
-    ],
+        // list of files / patterns to load in the browser
+        files: [
+            'bower_components/angular/angular.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+            //jquery, jasmine-jquery are here only for purpose of including mock json data
+            //now it's using approach suggested here:
+            // http://stackoverflow.com/questions/17370427/loading-a-mock-json-file-within-karmaangularjs-test
+            'bower_components/jquery/dist/jquery.min.js',
+            'node_modules/jasmine-jquery/lib/jasmine-jquery.js',
+            'js/*.js',
+            'test/*.js',
+            // fixtures to include mock data
+            {pattern: 'test/mock_data/*.json', watched: true, served: true, included: false}
+        ],
 
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+        // list of files to exclude
+        exclude: [],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {},
 
 
-    // web server port
-    port: 9876,
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['progress'],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // web server port
+        port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['Chrome'],
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        },
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
-}
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true,
+
+        // Concurrency level
+        // how many browser should be started simultaneous
+        concurrency: Infinity
+
+    };
+
+    if (process.env.TRAVIS) {
+        configuration.browsers = ['Chrome_travis_ci'];
+    }
+    config.set(configuration);
+};
