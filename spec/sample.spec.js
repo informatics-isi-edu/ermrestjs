5e6d30aba4fb4bda0f2db23bd6ea50b684084ed0
Chirag Sanghvi <csanghvi@appacitive.com>
2016-06-30 18:03:56 -0700

    Implemented simple test case to introspect a catalog with setup and tear

--- spec/sample.spec.js
+++ spec/sample.spec.js
@@ -1,29 +1,54 @@
 
-var url = "https://dev.isrd.isi.edu/ermrest",
-    authCookie = 'ermrest_cookie';
+var url = "https://dev.isrd.isi.edu/ermrest", cookie = "ermrest_cookie";
 
-var ermrestUtils = require("ermrest-data-utils");
+var includes = require(__dirname + '/utils/ermrest-init.js').init({
+    url: url,
+    authCookie : cookie
+});
 
-var ermRest = require(process.env.PWD + "/build/ermrest.js");
-ermRest.setUserCookie(authCookie);
+var server = includes.server;
+var ermRest = includes.ermRest;
+var ermrestUtils = includes.ermrestUtils;
 
 describe('In ERMrest,', function () {
-    var catalog_id = 1, schema, catalog;
-    var server = ermRest.ermrestFactory.getServer(url);
-
+    var catalog_id, schemaName, schema, catalog;
+    
     beforeAll(function (done) {
-        server.catalogs.get(1).then(function(response) {
-            catalog = response;
-            schema = catalog.schemas.get('legacy');
+        ermrestUtils.importData({
+            configuration: require('./configuration/sample.spec.conf.json'), 
+            url: url + "/",
+            authCookie : cookie
+        }).then(function(data) {
+            catalog_id = data.catalogId;
+            schemaName = data.schema.name;
+            console.log("Data imported with catalogId " + data.catalogId);
             done();
         }, function(err) {
+            console.log("Unable to import data");
             console.dir(err);
         });
     });
 
-    it('Should have schema name as legacy', function (done) {
+    it('should introspect catalog', function(done) {
+        expect(server.catalogs).toBeDefined();
+        expect(server.catalogs.get).toBeDefined();
+        server.catalogs.get(catalog_id).then(function(response) {
+            catalog = response;
+            expect(catalog).toBeDefined();
+            expect(catalog.schemas).toBeDefined();
+            expect(catalog.schemas.get).toBeDefined();
+            schema = catalog.schemas.get(schemaName);
+            done();
+        }, function(err) {
+            console.dir(err);
+            done.fail();
+        });
+    })
+
+    it('Should have schema name as ' + schemaName, function (done) {
         console.log("in test");
         expect(schema).toBeDefined();
+        expect(schema.name).toBe(schemaName);
         done();
     });
 
@@ -32,4 +57,18 @@ describe('In ERMrest,', function () {
         expect(catalog.id).toBe(catalog_id);
         done();
     });
+
+    afterAll(function(done) {
+        ermrestUtils.tear({
+            dataSetup: require('./configuration/sample.spec.conf.json'),
+            catalogId: catalog_id,
+            url:  url + "/",
+            authCookie : cookie
+        }).then(function(data) {
+            done();
+        }, function(err) {
+            console.log("Unable to import data");
+            console.dir(err);
+        });
+    })
 });
