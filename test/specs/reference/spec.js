3731bc66c84159954bf6cc0474f0efa3ed889968
Joshua Chudy <jchudy@isi.edu>
2016-07-26 12:13:37 -0700

    Test for reference for reading with a limit that is less than the range of entities available.

--- test/specs/reference/spec.js
+++ test/specs/reference/spec.js
@@ -4,9 +4,9 @@ var server = includes.server;
 var ermRest = includes.ermRest;
 var ermrestUtils = includes.ermrestUtils;
 
-describe("For determining reference objects and it's child objects, ", function () {
-    var catalog_id, schemaName, uri, reference, page, tuple;
-    var tableName = "reference_table", entityId = 9000, limit = 1;
+describe("For determining reference objects and it's child objects,", function () {
+    var catalog_id, schemaName, singleEnitityUri, multipleEntityUri;
+    var tableName = "reference_table", entityId = 9000, lowerLimit = 8999, upperLimit = 9010;
 
     // This function should be present in all spec files. It will add sample database and configurations.
     beforeAll(function (done) {
@@ -18,8 +18,11 @@ describe("For determining reference objects and it's child objects, ", function 
             catalog_id = data.catalogId;
             schemaName = data.schema.name;
 
-            uri = includes.url + "/catalog/" + catalog_id + "/entity/"
+            singleEnitityUri = includes.url + "/catalog/" + catalog_id + "/entity/"
                 + schemaName + ":" + tableName + "/id=" + entityId;
+
+            multipleEntityUri = includes.url + "/catalog/" + catalog_id + "/entity/" + schemaName + ":"
+                + tableName + "/id::gt::" + lowerLimit + "&id::lt::" + upperLimit;
 
             console.log("Data imported with catalog Id " + catalog_id);
             done();
@@ -31,82 +34,146 @@ describe("For determining reference objects and it's child objects, ", function 
     });
 
     // Test Cases:
-    it('resolve should return a Reference object that is defined.', function(done) {
-        ermRest.resolve(uri, {cid: "test"}).then(function (response) {
-            reference = response;
+    describe('for a single entity,', function() {
+        var reference, page, tuple;
+        var limit = 1;
 
-            expect(reference).toEqual(jasmine.any(Object));
+        it('resolve should return a Reference object that is defined.', function(done) {
+            ermRest.resolve(singleEnitityUri, {cid: "test"}).then(function (response) {
+                reference = response;
 
-            done();
-        }, function (err) {
-            console.dir(err);
-            done.fail();
+                expect(reference).toEqual(jasmine.any(Object));
+
+                done();
+            }, function (err) {
+                console.dir(err);
+                done.fail();
+            });
+        });
+
+        it('reference should be properly defined based on the constructor.', function() {
+            expect(reference._uri).toBe(singleEnitityUri);
+            expect(reference._serviceUrl).toBe(includes.url);
+            expect(reference._catalogId).toBe(catalog_id.toString());
+            expect(reference._schemaName).toBe(schemaName);
+            expect(reference._tableName).toBe(tableName);
+            expect(reference._filter instanceof ermRest.ParsedFilter).toBeDefined();
+        });
+
+        // Methods that are currently implemented
+        it('reference should have methods properly defined.', function() {
+            expect(reference.uri).toBe(reference._uri);
+            expect(reference.columns).toBe(reference._columns);
+            expect(reference.read()).toBeDefined();
+        });
+
+        it('reference should be properly defined after the callback is resolved.', function() {
+            expect(reference._catalog).toBeDefined();
+            expect(reference._schema).toBeDefined();
+            expect(reference._table).toBeDefined();
+            expect(reference._columns).toBeDefined();
+        });
+
+        // Single Entity specific tests
+        it('read should return a Page object that is defined.', function(done) {
+            reference.read(limit).then(function (response) {
+                page = response;
+
+                expect(page).toEqual(jasmine.any(Object));
+
+                done();
+            }, function (err) {
+                console.dir(err);
+                done.fail();
+            });
+        });
+
+        it('page should be properly defined based on constructor.', function() {
+            expect(page._ref).toBe(reference);
+            expect(page._data.length).toBe(limit);
+            expect(page._data[0].id).toBe(entityId.toString());
+        });
+
+        it('page should have methods properly defined.', function() {
+            expect(page.tuples).toBeDefined();
+        });
+
+        it('tuples should return an Array of Tuple objects.', function() {
+            tuple = page.tuples[0];
+            expect(page._tuples).toBeDefined();
+            expect(tuple._data).toBe(page._data[0]);
+            expect(tuple._ref).toBe(reference);
+        });
+
+        it('tuples should have methods properly defined.', function() {
+            expect(tuple.values).toBeDefined();
+        });
+
+        it('values should return only the values of the tuple.', function() {
+            var values = tuple.values;
+            // based on order in reference_table.json
+            expect(values[0]).toBe(tuple._data.id);
+            expect(values[1]).toBe(tuple._data.name);
+            expect(values[2]).toBe(tuple._data.value.toString());
         });
     });
 
-    it('reference should be properly defined based on the constructor.', function() {
-        expect(reference._uri).toBe(uri);
-        expect(reference._serviceUrl).toBe(includes.url);
-        expect(reference._catalogId).toBe(catalog_id.toString());
-        expect(reference._schemaName).toBe(schemaName);
-        expect(reference._tableName).toBe(tableName);
-        expect(reference._filter instanceof ermRest.ParsedFilter).toBeDefined();
-    });
+    describe('for multiple entities,', function() {
+        var reference, page, tuple;
+        var limit = 5;
 
-    // Methods that are currently implemented
-    it('reference should have methods properly defined.', function() {
-        expect(reference.uri).toBe(reference._uri);
-        expect(reference.columns).toBe(reference._columns);
-        expect(reference.read()).toBeDefined();
-    });
+        it('resolve should return a Reference object that is defined.', function(done) {
+            ermRest.resolve(multipleEntityUri, {cid: "test"}).then(function (response) {
+                reference = response;
 
-    it('reference should be properly defined after the callback is resolved.', function() {
-        expect(reference._catalog).toBeDefined();
-        expect(reference._schema).toBeDefined();
-        expect(reference._table).toBeDefined();
-        expect(reference._columns).toBeDefined();
-    });
+                expect(reference).toEqual(jasmine.any(Object));
 
-    it('read should return a Page object that is defined.', function(done) {
-        reference.read(limit).then(function (response) {
-            page = response;
-
-            expect(page).toEqual(jasmine.any(Object));
-
-            done();
-        }, function (err) {
-            console.dir(err);
-            done.fail();
+                done();
+            }, function (err) {
+                console.dir(err);
+                done.fail();
+            });
         });
-    });
 
-    it('page should be properly defined based on constructor.', function() {
-        expect(page._ref).toBe(reference);
-        expect(page._data.length).toBe(1);
-        expect(page._data[0].id).toBe(entityId.toString());
-    });
+        it('read should return a Page object that is defined.', function(done) {
+            reference.read(limit).then(function (response) {
+                page = response;
 
-    it('page should have methods properly defined.', function() {
-        expect(page.tuples).toBeDefined();
-    });
+                expect(page).toEqual(jasmine.any(Object));
 
-    it('tuples should return an Array of Tuple objects.', function() {
-        tuple = page.tuples[0];
-        expect(page._tuples).toBeDefined();
-        expect(tuple._data).toBe(page._data[0]);
-        expect(tuple._ref).toBe(reference);
-    });
+                done();
+            }, function (err) {
+                console.dir(err);
+                done.fail();
+            });
+        });
 
-    it('tuples should have methods properly defined.', function() {
-        expect(tuple.values).toBeDefined();
-    });
+        it('page should be properly defined based on constructor.', function() {
+            expect(page._ref).toBe(reference);
+            expect(page._data.length).toBe(limit);
+            expect(page._data[0].id).toBe((lowerLimit + 1).toString());
+        });
 
-    it('values should return only the values of the tuple.', function() {
-        var values = tuple.values;
-        // based on order in reference_table.json
-        expect(values[0]).toBe('9000');
-        expect(values[1]).toBe('Hank');
-        expect(values[2]).toBe('12');
+        it('tuples should return an Array of Tuple objects.', function() {
+            tuples = page.tuples;
+
+            expect(page._tuples).toBeDefined();
+            expect(tuples.length).toBe(page._data.length);
+            expect(tuples.length).toBe(limit);
+            expect(tuples[0]._data).toBe(page._data[0]);
+            expect(tuples[4]._data).toBe(page._data[4]);
+        });
+
+        it('values should return only the values of the tuple.', function() {
+            for(var i = 0; i < limit; i++) {
+                var tuple = tuples[i];
+                var values = tuple.values;
+                // based on order in reference_table.json
+                expect(values[0]).toBe(tuple._data.id);
+                expect(values[1]).toBe(tuple._data.name);
+                expect(values[2]).toBe(tuple._data.value.toString());
+            }
+        });
     });
 
     // This function should be present in all spec files. It will remove the newly created catalog
