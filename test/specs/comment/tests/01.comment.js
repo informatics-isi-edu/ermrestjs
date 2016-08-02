7273306088a4e167c4f7b433fe5ceb084cd27cee
Jessie Wong <jessie@thisisjessie.com>
2016-08-02 15:29:09 -0700

    Add assertions to .comment tests. Check if .comment property exists on models, and check .comment is null if no comment in schema

--- test/specs/comment/tests/01.comment.js
+++ test/specs/comment/tests/01.comment.js
@@ -10,21 +10,44 @@ exports.execute = function (options) {
 
         // Test Cases:
         it('schema_with_comment should have a non-null .comment property.', function () {
+            expect(schema.hasOwnProperty('comment')).toBe(true);
             expect(schema.comment).toBe("schema with a comment");
         });
 
         it('table_with_comment should have a non-null .comment property.', function () {
-            expect(schema.tables.get('table_with_comment').comment).toBe("table with a comment");
+            var table = schema.tables.get('table_with_comment');
+            expect(table.hasOwnProperty('comment')).toBe(true);
+            expect(table.comment).toBe("table with a comment");
+        });
+
+        it('table_with_null_comment should have a null .comment property.', function () {
+            var table = schema.tables.get('table_with_null_comment');
+            expect(table.hasOwnProperty('comment')).toBe(true);
+            expect(table.comment).toBe(null);
         });
 
         it('this key with a comment should have a non-null .comment property.', function () {
             var key = schema.tables.get('table_with_comment').keys.all()[0];
+            expect(key.hasOwnProperty('comment')).toBe(true);
             expect(key.comment).toBe("key with a comment");
+        });
+
+        it('this key in table_with_comment should have a null .comment property.', function() {
+            var key = schema.tables.get('table_with_null_comment').keys.all()[0];
+            expect(key.hasOwnProperty('comment')).toBe(true);
+            expect(key.comment).toBe(null);
         });
 
         it('this foreign key with a comment should have a non-null .comment property.', function () {
             var fkey = schema.tables.get('table_with_comment').foreignKeys.all()[0];
+            expect(fkey.hasOwnProperty('comment')).toBe(true);
             expect(fkey.comment).toBe("foreign key with a comment");
+        });
+
+        it('this foreign key in table_with_null_comment should have a null .comment property.', function() {
+            var fkey = schema.tables.get('table_with_null_comment').foreignKeys.all()[0];
+            expect(fkey.hasOwnProperty('comment')).toBe(true);
+            expect(fkey.comment).toBe(null);
         });
     });
 };
