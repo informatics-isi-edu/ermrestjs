0c62cdfb745e1d79a201e3e2c03db89f4761cb5c
Aref Shafaei <aref.shafaei@gmail.com>
2016-08-04 15:24:41 -0700

    Add check for oder in visible foreign keys

--- test/specs/annotation/tests/04.visible_foreign_keys.js
+++ test/specs/annotation/tests/04.visible_foreign_keys.js
@@ -13,12 +13,12 @@ exports.execute = function (options) {
 
         // Test Cases:
         // Helper function to add 'expect' based on test cases and the specified table
-        function runTestCases(table, cases) {
+        function runTestCases(table, cases, check_order) {
             Object.keys(cases).forEach(function (context) {
                 var fk_names = schema.tables.get(table)._visibleForeignKeys(context).map( function(fk){
                     return fk.constraint_names[0][1]; // return the actual constraint_name
                 });
-                expect(fk_names).toHaveSameItems(cases[context], true);
+                expect(fk_names).toHaveSameItems(cases[context], !check_order);
             });
         }
 
@@ -28,25 +28,25 @@ exports.execute = function (options) {
                 "entry": ["fk_with_annotation_to_outbound"],
                 "edit": ["fk_inbound_to_with_annotation"],
                 "*": []
-            });
+            }, true);
         });
 
         it('should use `entry` context when context is `edit` or `create` and they are not present in annotation.', function () {
             runTestCases(tableWithAnnotation, {
                 "create": ["fk_with_annotation_to_outbound"]
-            });
+            }, true);
         }); 
         
         it('should use default (`*`) context for any context not matched by a more specific context name.', function () {
             runTestCases(tableWithAnnotation, {
                 "filter": []
-            });
+            }, true);
         });
 
         it('should ignore the constraint names that do not correspond to any inbound or outbound foreign key of the table.', function () {
             runTestCases(tableWithAnnotation, {
                 "record": ["fk_inbound_to_with_annotation"] 
-            });
+            }, true);
         });
 
         it('should ignore repetitive constraint names and just consider the first one.', function () {
@@ -55,7 +55,7 @@ exports.execute = function (options) {
                     "fk_inbound_to_with_annotation",
                     "fk_with_annotation_to_outbound"
                 ] 
-            });
+            }, true);
         });
 
         it('should use the default order when context not matched by a more specific context name and default (`*`) context is not present.', function () {
@@ -64,7 +64,7 @@ exports.execute = function (options) {
                     "fk_inbound_to_without_annotation",
                     "fk_without_annotation_to_outbound"
                 ] 
-            });
+            }, false);
         });
 
 
