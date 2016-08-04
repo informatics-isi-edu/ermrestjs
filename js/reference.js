aa62dbcb5285074fa7092f073df69bfc0db89b68
Shinyi Chen <jchen@isi.edu>
2016-08-03 18:04:57 -0700

    Implemented default Reference.displayname.

--- js/reference.js
+++ js/reference.js
@@ -158,7 +158,9 @@ var ERMrest = (function(module) {
              * In one directoin, the FKR is named "parent" in the other
              * direction it is named "child".
              */
-            this._displayname = this._table.displayname();
+            // TODO related reference displayname
+            if (!this._displayname)
+                this._displayname = this._table.displayname;
             return this._displayname;
         },
 
