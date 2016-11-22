f85c3ff4598c7f763892661a1472278f395cf938
Aref Shafaei <aref.shafaei@gmail.com>
2016-11-22 15:36:18 -0800

    remove visible columns test cases as they are not relevant

--- test/specs/annotation/spec.js
+++ test/specs/annotation/spec.js
@@ -2,13 +2,11 @@ require('./../../utils/starter.spec.js').runTests({
     description: 'In annotations, ',
     testCases: [
         "/annotation/tests/01.displayname.js",
-        '/annotation/tests/02.visible_columns.js',
         "/annotation/tests/03.table_display.js",
         "/annotation/tests/04.visible_foreign_keys.js"
     ],
     schemaConfigurations: [
         "/annotation/conf/displayname.conf.json",
-        "/annotation/conf/visible_columns.conf.json",
         "/annotation/conf/table_display.conf.json",
         "/annotation/conf/visible_foreign_keys.json"
     ]
