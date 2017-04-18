af26bd16c9121b615318bcfc03baeef301a4a968
Chirag Sanghvi <csanghvi@appacitive.com>
2017-04-18 14:05:20 -0700

    Added documentation for 401 handling

--- js/http.js
+++ js/http.js
@@ -77,7 +77,7 @@ var ERMrest = (function (module) {
     /**
      * function that is called when a HTTP 401 Error occurs
      * @callback httpUnauthorizedFn
-     * @type {httpUnauthorizedFn}
+     * @type {httpUnauthorizedFn}: Should return a promise
      * @private
      */
     module._httpUnauthorizedFn = null;
@@ -91,7 +91,17 @@ var ERMrest = (function (module) {
         module._httpUnauthorizedFn = fn;
     };
 
+    /*
+     * A flag to determine whether emrest authorization error has occured 
+     * as well as to determine the login flow is currently in progress to avoid 
+     * calling the _httpUnauthorizedFn callback again
+     */
     var _ermrestAuthorizationFailureFlag = false;
+    
+    /* 
+     * All the calls that were paused because of 401 error are added to this array
+     *  Once the _ermrestAuthorizationFailureFlag is false, all of them will be resolved/restarted
+    */
     var _authorizationDefers = [];
 
     /**
@@ -104,6 +114,8 @@ var ERMrest = (function (module) {
     module._onHttpAuthFlowFn = function() {
         var defer = module._q.defer();
 
+        // If _ermrestAuthorizationFailureFlag is true then push the defer to _authorizationDefers
+        // else just resolve it directly
         if (_ermrestAuthorizationFailureFlag) _authorizationDefers.push(defer);
         else defer.resolve();
 
@@ -187,16 +199,24 @@ var ERMrest = (function (module) {
                             });
                         } else if (response.status == 401) {
 
+                            // If _ermrestAuthorizationFailureFlag is not set then
                             if (_ermrestAuthorizationFailureFlag === false) {
                                 
+                                // If callback has been registered in _httpUnauthorizedFn
                                 if (typeof module._httpUnauthorizedFn == 'function') {
                                     
+                                    // Set _ermrestAuthorizationFailureFlag to avoid the handler from being called again
                                     _ermrestAuthorizationFailureFlag = true;
 
+                                    // Push the current call to _authroizationDefers by calling _onHttpAuthFlowFn
                                     module._onHttpAuthFlowFn().then(function() {
                                         asyncfn();
                                     });
                                     
+                                    // Call the handler, which will return a promise
+                                    // On success set the flag as false and resolve all the authorizationDefers
+                                    // So that other calls which failed due to 401 or were trigerred after the 401
+                                    // are reexecuted
                                     module._httpUnauthorizedFn().then(function() {
                                 
                                         _ermrestAuthorizationFailureFlag = false;
@@ -221,6 +241,9 @@ var ERMrest = (function (module) {
                     });
                 }
 
+                // Push the current call to _authorizationDefers by calling _onHttpAuthFlowFn
+                // If the _ermrestAuthorizationFailureFlag is false then asyncfn will be called immediately
+                // else it will be queued
                 module._onHttpAuthFlowFn().then(function() {
                     asyncfn();
                 });
