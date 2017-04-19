34fa9b4f8b1c678c1a91bc65c4cf460f59da4528
Joshua Chudy <jchudy@isi.edu>
2017-04-19 14:40:23 -0700

    make sure content type header is available before trying to take an action on it

--- js/http.js
+++ js/http.js
@@ -196,7 +196,7 @@ var ERMrest = (function (module) {
 
                              // If we get an HTTP error with HTML in it, this means something the server returned as an error.
                              // Ermrest never produces HTML errors, so this was produced by the server itself
-                             if (response.headers()['content-type'].indexOf("html") > -1) {
+                             if (response.headers()['content-type'] && response.headers()['content-type'].indexOf("html") > -1) {
                                  response.status = response.statusCode = _http_status_codes.service_unavailable;
                                  response.data = "An unexpected error has occurred. Please report this problem to your system administrators.";
                              } else {
@@ -245,7 +245,7 @@ var ERMrest = (function (module) {
                         } else {
                             // If we get an HTTP error with HTML in it, this means something the server returned as an error.
                             // Ermrest never produces HTML errors, so this was produced by the server itself
-                            if (response.headers()['content-type'].indexOf("html") > -1) {
+                            if (response.headers()['content-type'] && response.headers()['content-type'].indexOf("html") > -1) {
                                 response.status = response.statusCode = _http_status_codes.service_unavailable;
                                 response.data = "An unexpected error has occurred. Please report this problem to your system administrators.";
                             }
