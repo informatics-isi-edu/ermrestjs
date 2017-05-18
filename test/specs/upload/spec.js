fe9f0bf0eb28ddd55a1dc334f93c8e4840f3693a
Chirag Sanghvi <csanghvi@appacitive.com>
2017-05-18 15:15:32 -0700

    Added test cases for upload

--- test/specs/upload/spec.js
+++ test/specs/upload/spec.js
@@ -1,7 +1,8 @@
 require('./../../utils/starter.spec.js').runTests({
     description: 'In upload spec, ',
     testCases: [
-        "/upload/tests/01.checksum.js"
+        "/upload/tests/01.checksum.js",
+        "/upload/tests/02.upload_process.js"
     ],
     schemaConfigurations: [
         "/upload/conf/upload.conf.json"
