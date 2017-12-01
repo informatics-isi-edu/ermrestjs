36c518a68dbbf03c97142009dae848a06b460411
Chirag Sanghvi <csanghvi@appacitive.com>
2017-12-01 15:18:53 -0800

    Added some documentation for fomat

--- js/format.js
+++ js/format.js
@@ -1,5 +1,6 @@
 var ERMrest = (function(module) {
 
+    // A list of regular expression to extract flags, width and precision from format strings
     var re = {
         number: /[dief]/,
         numeric_arg: /[bcdiefuX]/,
@@ -235,7 +236,57 @@ var ERMrest = (function(module) {
         return parse_tree;
     };
 
+    /**
+      * printf function allows you to format strings using POSIX statndard
+      *
+      * %[flags][width][.precision]type
+      *
+      * flags field: The Flags field can be zero or more (in any order) of:
+    
+           - (minus) Left-align the output of this placeholder. (The default is to right-align the output.). 
+                     This is used in conjunction with width field.
+           + (plus)  Prepends a plus for positive signed-numeric types. positive = +, negative = -
+                     (The default doesn't prepend anything in front of positive numbers.)
+           0 (zero)  When the 'width' option is specified, prepends zeros for numeric types. (The default prepends spaces.)
+                     For example, printf("%2X",3) produces  3, while printf("%02X",3) produces in 03.
+           ' (quote) It separates numeric types by thousands using a comman(,) or other characters accroding to localization 
+           #{char}   An optional padding specifier that says what character to use for padding (if specified). 
+                     Possible values are any other character precedeed by a # (hash). print("%#_5d",10) produces ___10
+    
+      * Width field: The Width field specifies a minimum number of characters to output, and is typically used to pad fixed-width fields, 
+                     where the fields would otherwise be smaller, although it does not cause truncation of oversized fields.
+                     For example, printf("%*d", 5, 10) will result in "   10" being printed, with a total width of 5 characters.
+                     Though not part of the width field, a leading zero is interpreted as the zero-padding flag mentioned above, 
+                     and a negative value is treated as the positive value in conjunction with the left-alignment - flag also mentioned above.
+                     When used with the j (JSON) type specifier, the padding length specifies the tab size used for indentation.
+      
+      * Precision field: The Precision field usually specifies a maximum limit on the output, depending on the particular formatting type.
+                    For floating point numeric types, it specifies the number of digits to the right of the decimal point that the output should be rounded.
+                    For the string type, it limits the number of characters that should be output, after which the string is truncated.
+                    For example, printf("%.3s", 3.42134) will result in 3.421 being printed.
+        
+      * Type field: It can be any of:
+                    % — yields a literal % character
+                    b — yields an integer as a binary number
+                    c — yields an integer as the character with that ASCII value
+                    d or i — yields an integer as a signed decimal number
+                    e — yields a float using scientific notation
+                    u — yields an integer as an unsigned decimal number
+                    f — yields a float as is; see notes on precision above
+                    o — yields an integer as an octal number
+                    s — yields a string as is
+                    t — yields true or false
+                    X — yields an integer as a hexadecimal number (upper-case)
+                    j — yields a JavaScript object or array as a JSON encoded string
+
+       * @param{Object} options A javascript object that contains format property and optional bool_true_value
+                                and bool_false_value. These will be used to replace true and false in `t` type fields.
+       * @param{String|Number|Object} value This would be the value that needs to be formatted.
+
+       * @returns {String}
+       **/
     module._printf = function(options, value) {
+        if (typeof options.format !== 'string') throw new SyntaxError("[printf] should be supplied with proper per_format annotation with format string. Eg: { format: '%d' }");
         return printf_format(printf_parse(options.format), value, options);
     };
 
