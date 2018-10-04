9f151c89c97ae7acbf321e4c5ad1dd8d37395b16
Josh Chudy <jchudy@isi.edu>
2018-10-04 13:32:38 -0700

    added catalog id and version to location object (#721)
    added catalog id and version to location object

--- js/parser.js
+++ js/parser.js
@@ -106,7 +106,11 @@
         this._service = parts[1];
 
         // catalog id
-        this._catalog = parts[2];
+        this._catalogSnapshot = parts[2];
+
+        var catalogParts = this._catalogSnapshot.split('@');
+        this._catalog = catalogParts[0];
+        this._version = catalogParts[1] || null;
 
         // api
         this._api = parts[3];
@@ -491,7 +495,23 @@
          * @returns {String} catalog id
          */
         get catalog() {
+            return this._catalogSnapshot;
+        },
+
+        /**
+         *
+         * @returns {String} just the catalog id
+         */
+        get catalogId() {
             return this._catalog;
+        },
+
+        /**
+         *
+         * @returns {String} just the catalog version
+         */
+        get version() {
+            return this._version;
         },
 
         /**
