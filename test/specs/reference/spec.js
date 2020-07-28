a3c5c387236fe1ea65d6195be6c25a1fca242126
Joshua Chudy <jchudy@isi.edu>
2020-07-28 11:19:05 -0700

    forgot a comma

--- test/specs/reference/spec.js
+++ test/specs/reference/spec.js
@@ -34,7 +34,7 @@ require('./../../utils/starter.spec.js').runTests({
         "/reference/conf/delete.conf.json",
         "/reference/conf/default_value.conf.json",
         "/reference/conf/aggregate.conf.json",
-        "/reference/conf/permission.conf.json"
+        "/reference/conf/permission.conf.json",
         "/reference/conf/comment_display_inline.conf.json",
         "/reference/conf/comment_display_related.conf.json"
     ]
