0a0555736378270cf9d8c8b30e42b70eacf81359
KPherwani <karishma.pherwani21@gmail.com>
2021-07-20 12:44:01 -0700

    1801 - code review changes

--- test/specs/json_ld/tests/02.json_ld_validator.js
+++ test/specs/json_ld/tests/02.json_ld_validator.js
@@ -2,40 +2,36 @@
 exports.execute = function (options) {
     var module = options.includes.ermRest;
     describe("validation of structured data", function () {
-        var jsonInputOrig = require("../resources/input_json_ld.json");
-
-        it("The google_metadata with incorrect attribute 'verson' returned w/o it", function (done) {
-            var jsonInput = Object.assign({}, jsonInputOrig);
-            jsonInput["verson"] = "4";
+        it("should ignore attributes that are not part of the vocabulary", function (done) {
+            var jsonInput = {"@type": "Dataset","@context": "http://schema.org","name": "dummy", 
+            "description": "lorem ipsum", "verson": "4"};
             expect(jsonInput.verson).toEqual("4");
 
             var result = module.validateJSONLD(jsonInput);
             expect(result.isValid).toEqual(true);
             expect(result.modifiedJsonLd.verson).toEqual(undefined);
-            expect(result.modifiedJsonLd).toEqual(jsonInputOrig);
+            delete jsonInput["verson"];
+            expect(result.modifiedJsonLd).toEqual(jsonInput);
             done();
         });
 
-        it("The google_metadata with missing @context returned with correct @context", function (done) {
-            var jsonInput = Object.assign({}, jsonInputOrig);
-            expect(jsonInput["@context"]).toEqual("https://schema.org");
-            delete jsonInput["@context"];
+        it("should return false if missing @context", function (done) {
+            var jsonInput = {"@type": "Dataset","name": "dummy", 
+            "description": "lorem ipsum"};
             expect(jsonInput["@context"]).toEqual(undefined);
-            expect(jsonInput).not.toEqual(jsonInputOrig);
 
             var result = module.validateJSONLD(jsonInput);
             expect(result.isValid).toEqual(true);
             expect(result.modifiedJsonLd["@context"]).toEqual("https://schema.org");
-            expect(result.modifiedJsonLd).toEqual(jsonInputOrig);
+            jsonInput["@context"] = "https://schema.org";
+            expect(result.modifiedJsonLd).toEqual(jsonInput);
             done();
         });
 
-        it("The google_metadata with incorrect @type returned false", function (done) {
-            var jsonInput = Object.assign({}, jsonInputOrig);
-            expect(jsonInput["@type"]).toEqual("Dataset");
-            jsonInput["@type"] = "Datass";
+        it("should return false if incorrect @type", function (done) {
+            var jsonInput = {"@type": "Datass","@context": "http://schema.org","name": "dummy", 
+            "description": "lorem ipsum"};
             expect(jsonInput["@type"]).toEqual("Datass");
-            expect(jsonInput).not.toEqual(jsonInputOrig);
 
             var result = module.validateJSONLD(jsonInput);
             expect(result.isValid).toEqual(false);
@@ -43,49 +39,62 @@ exports.execute = function (options) {
         });
 
         it("The google_metadata with missing required attribute 'name' inside 'creator' returned without 'creator'", function (done) {
-            var jsonInput = Object.assign({}, jsonInputOrig);
-            expect(jsonInput["creator"]["name"]).toEqual("Statista");
-            delete jsonInput["creator"]["name"];
+            var jsonInput = {
+                "@type": "Dataset", "@context": "http://schema.org", "name": "dummy",
+                "description": "lorem ipsum", "creator": {
+                    "@type": "Organization",
+                    "url": "https://www.statista.com",
+                    "address": "los angeles",
+                    "logo": {
+                        "@type": "ImageObject",
+                        "url": "https://cdn.statcdn.com/static/img/Statista-Logo.png"
+                    }
+                }
+            };
             expect(jsonInput["creator"]["name"]).toEqual(undefined);
             expect(jsonInput["creator"]).toBeObject();
 
             var result = module.validateJSONLD(jsonInput);
             expect(result.isValid).toEqual(true);
             expect(result.modifiedJsonLd["creator"]).not.toBeObject();
-            expect(result.modifiedJsonLd).not.toEqual(jsonInputOrig);
+            delete jsonInput["creator"];
+            expect(result.modifiedJsonLd).toEqual(jsonInput);
             done();
         });
 
-        it("The google_metadata with missing required attribute returned as false", function (done) {
-            var jsonInput = Object.assign({}, jsonInputOrig);
-            expect(jsonInput["name"]).toEqual("Sales of the leading ice cream brands of the U.S. 2020");
-            delete jsonInput["name"];
+        it("should return false if missing required vocabulary keyword", function (done) {
+            var jsonInput = {"@type": "Dataset","@context": "http://schema.org", "description": "lorem ipsum"};
             expect(jsonInput["name"]).toEqual(undefined);
-            expect(jsonInput).not.toEqual(jsonInputOrig);
 
             var result = module.validateJSONLD(jsonInput);
             expect(result.isValid).toEqual(false);
             done();
         });
 
-        it("The google_metadata with incorrect datatype of attribute, returned w/o that attribute", function (done) {
-            var jsonInput = Object.assign({}, jsonInputOrig);
-            expect(jsonInput["url"]).toEqual("https://www.statista.com/statistics/190426/top-ice-cream-brands-in-the-united-states/");
-            jsonInput["url"] = 77;
+        it("should ignore attribute if datatype not followed", function (done) {
+            var jsonInput = {"@type": "Dataset","@context": "http://schema.org","name": "dummy", 
+            "description": "lorem ipsum", "url": 77};
             expect(jsonInput["url"]).toEqual(77);
-            expect(jsonInput).not.toEqual(jsonInputOrig);
 
             var result = module.validateJSONLD(jsonInput);
             expect(result.isValid).toEqual(true);
             expect(result.modifiedJsonLd["url"]).toEqual(undefined);
-            expect(result.modifiedJsonLd).not.toEqual(jsonInputOrig);
+            delete jsonInput["url"];
+            expect(result.modifiedJsonLd).toEqual(jsonInput);
             done();
         });
 
         it("The google_metadata with incorrect element(incorrect data type) inside array returned with that element excluded", function (done) {
-            var jsonInput = Object.assign({}, jsonInputOrig);
-            expect(jsonInput["keywords"].length).toEqual(4);
-            jsonInput["keywords"].push(22);
+            var jsonInput = {
+                "@type": "Dataset", "@context": "http://schema.org", "name": "dummy",
+                "description": "lorem ipsum", 
+                "keywords": ["sales",
+                    "ice cream",
+                    "ice cream brands",
+                    "brands",
+                    22
+                ]
+            };
             expect(jsonInput["keywords"].length).toEqual(5);
             expect(jsonInput["keywords"][4]).toEqual(22);
             expect(jsonInput["keywords"][3]).toEqual("brands");
@@ -96,11 +105,13 @@ exports.execute = function (options) {
             var result = module.validateJSONLD(jsonInput);
             expect(result.isValid).toEqual(true);
             expect(result.modifiedJsonLd["keywords"].length).toEqual(4);
-            expect(jsonInput["keywords"][4]).toEqual(undefined);
-            expect(jsonInput["keywords"][3]).toEqual("brands");
-            expect(jsonInput["keywords"][2]).toEqual("ice cream brands");
-            expect(jsonInput["keywords"][1]).toEqual("ice cream");
-            expect(jsonInput["keywords"][0]).toEqual("sales");
+            expect(result.modifiedJsonLd["keywords"][4]).toEqual(undefined);
+            expect(result.modifiedJsonLd["keywords"][3]).toEqual("brands");
+            expect(result.modifiedJsonLd["keywords"][2]).toEqual("ice cream brands");
+            expect(result.modifiedJsonLd["keywords"][1]).toEqual("ice cream");
+            expect(result.modifiedJsonLd["keywords"][0]).toEqual("sales");
+            delete jsonInput["keywords"][4];
+            expect(result.modifiedJsonLd).toEqual(jsonInput);
             done();
         });
     });
